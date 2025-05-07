c75ac0ce248a6e335a4a7cec5eb9c58cfc0b8800
Vince-arch <vincekimani006@gmail.com>
2025-05-07 20:46:27 +0300

    Adding nav link for home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export const Navbar = () => {
   return (
     <>
  <div className="navbar h-20 shadow bg-gray-300 flex justify-between px-6 sticky top-0 z-50">
-  <div className="text-3xl font-bold">Topwall</div>
+  <Link to="/" className="text-3xl font-bold">Topwall</Link>
   <div className="flex gap-6">
     <a href="/about" className="text-lg text-gray-500 hover:text-gray-900">About</a>
     <a href="/products" className="text-lg text-gray-500 hover:text-gray-900">Products</a>
