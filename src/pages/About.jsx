ffbec7028c31f5c9ac79dec69b15ec4f1cad3f4f
Vince-arch <vincekimani006@gmail.com>
2025-05-07 22:00:51 +0300

    new image added to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import car from '../assets/car.jpg'
 const About = () => {
   return (
     <div>
-      
+      <div name='intro'>
+        <img 
+        src={car}
+        alt='car'
+        className='h-screen w-screen'
+        />
+      </div>
     </div>
   )
 }
