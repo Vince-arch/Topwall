7985c547a7d4e0201755c6886c1d155886c176ed
Vince-arch <vincekimani006@gmail.com>
2025-06-06 22:03:29 +0300

    adding products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Make sure you're using react-router
+import { Link } from 'react-router-dom';
 
 const policies = [
   { id: 1, name: "Automobile Insurance", description: "Coverage for motor vehicles and drivers." },
