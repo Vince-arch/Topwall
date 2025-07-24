67ed3a72674c4da1f92c702f1d06c361f062d28a
Vince-arch <vincekimani006@gmail.com>
2025-07-24 14:28:24 +0300

    fixing typos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,13 @@ import side from '../assets/side.jpg'
 
 
 export const Home = () => {
-  return (    
-      <>
+  return (
+    <>
       <div name='intro' className='relative group'>
-        <img 
-          src={intro} 
-          alt='intro image' 
-          className='w-screen h-screen flex items-center justify-center text-white transition-all duration-300' 
+        <img
+          src={intro}
+          alt='intro image'
+          className='w-screen h-screen flex items-center justify-center text-white transition-all duration-300'
         />
         <div className="absolute inset-0 bg-black opacity-50 group-hover:opacity-60 transition-opacity duration-300 flex items-center justify-center">
           <div className="text-white text-center px-4 opacity-0 group-hover:opacity-150 transform group-hover:translate-y-0 translate-y-10 transition-all duration-500 ease-out">
@@ -21,22 +21,22 @@ export const Home = () => {
               Protecting what matters most — tailored insurance solutions you can trust.
             </p>
           </div>
-        </div> 
+        </div>
       </div>
       <div name='about' className='m-5 flex gap-6 items-center justify-center'>
-      <img 
-          src={side} 
-          alt='intro image' 
-          className='w-full h-full flex rounded-2xl' 
+        <img
+          src={side}
+          alt='intro image'
+          className='w-full h-full flex rounded-2xl'
         />
 
         <p className='flex '>
-        Established in 2002, Topwall Insurance Agency Ltd is a reputable
-        insurance agency based in Nakuru, Kenya. They specialize in tailoring
-        insurance policies to meet individual needs, ensuring cost-effective
-        coverage and efficient claim settlements. Their comprehensive services
-        span various sectors, including land, air, and marine insurance.
-      </p>
+          Established in 2002, Topwall Insurance Agency Ltd is a reputable
+          insurance agency based in Nakuru, Kenya. We specialize in tailoring
+          insurance policies to meet individual needs, ensuring cost-effective
+          coverage and efficient claim settlements. Our comprehensive services
+          span various sectors, including land, air, and marine insurance.
+        </p>
       </div>
       <div name='services'>
 
@@ -44,6 +44,6 @@ export const Home = () => {
       <div name='testimonials'>
 
       </div>
-      </> 
+    </>
   )
 }
