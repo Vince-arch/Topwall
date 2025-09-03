f2ac0c4c79a2327d7d1e9738acad4dbc5c514171
Vince-arch <vincekimani006@gmail.com>
2025-09-03 16:31:21 +0300

    adding process.env

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  define: {
+    'process.env': process.env
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
